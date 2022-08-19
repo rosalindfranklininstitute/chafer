73a606cdf6bcc6eeaa98c15658b0bbb86322b3ad
perdigao1 <Luis Perdigao>
2022-08-19 11:11:21 +0100

    support for collecting opt params fitting data

--- chafer/ch_art_suppr_tools.py
+++ chafer/ch_art_suppr_tools.py
@@ -176,15 +176,16 @@ class cls_charge_artifact_suppression_filter():
         self.data_fit_max_length_edge_px = data_fit_max_length_edge_px
         self.data_fit_min_length_px = data_fit_min_length_px
 
-    def fermidirac_left(self, x, x0, a0, sigma0):
+    # Note to undersand these functions: charging artifacts typically appear with lower Z than background.
+    def fermidirac_left(self, x, x0, a0, sigma0): # ‾‾\__
         func = a0 *( 1.0/ (np.exp((x-x0)/sigma0) + 1) -1.0)
         return func
     
-    def fermidirac_right(self, x, x0, a0, sigma0):
+    def fermidirac_right(self, x, x0, a0, sigma0): # __/‾‾
         func = a0 *( - 1.0/ (np.exp((x-x0)/sigma0) + 1) )
         return func
 
-    def fermidirac_right_left(self, x, x0, a0, sigma0, x1, a1,sigma1):
+    def fermidirac_right_left(self, x, x0, a0, sigma0, x1, a1,sigma1): # __/‾‾ ... ‾‾\__
         func = self.fermidirac_right(x,x0,a0,sigma0) + self.fermidirac_left(x,x1,a1,sigma1) 
         return func
 
@@ -214,7 +215,7 @@ class cls_charge_artifact_suppression_filter():
         return edge_indices, out_intervals, in_intervals
     
 
-    def charge_artifact_FD_filter_downup_av_prevlines3_irow_pass( self, data0, data_labels0, irow0, pass_dir0 = PASS_DIR_DOWN ):
+    def charge_artifact_FD_filter_downup_av_prevlines3_irow_pass( self, data0, data_labels0, irow0, pass_dir0 = PASS_DIR_DOWN):
     
         errdata_xvalues_yvalues = []
 
@@ -229,6 +230,8 @@ class cls_charge_artifact_suppression_filter():
         edge_indices0 , out_intervals0, in_intervals0 = self.row_get_intervals(labels_row0)
         
         curline = (data0[irow0,:]).astype(np.float32)
+
+        opts = []
 
         if len(edge_indices0)>0:
 
@@ -282,11 +285,12 @@ class cls_charge_artifact_suppression_filter():
                         #print(f"Exception.")
                         #print(str(e))
                         errdata_xvalues_yvalues.append([irow0, x_values,y_values])
-                    else:
+                    else: #if there is no error in the optimization execute this
                         y_values_fit = func0( x_values, *popt )
                         curline[ i0:i1 ] -= y_values_fit
-        
-        return curline.astype(data0.dtype), errdata_xvalues_yvalues
+                        opts.append([irow0, func0.__name__, popt])
+
+        return curline.astype(data0.dtype), errdata_xvalues_yvalues, opts
 
     #Filter whole slice (2D) in two passes
     def charge_artifact_FD_filter_downup_av_prevlines3_2d(self, data_2d, data_labels_2d):
@@ -295,6 +299,7 @@ class cls_charge_artifact_suppression_filter():
 
         pass_dirs = [self.PASS_DIR_DOWN, self.PASS_DIR_UP]
 
+        opts = []
         for pass_dir in pass_dirs:
             #line-by-line
             for j in range(self.nlinesaverage, data_2d.shape[0]):
@@ -304,18 +309,22 @@ class cls_charge_artifact_suppression_filter():
                 if pass_dir == self.PASS_DIR_UP:
                     irow = data_2d.shape[0]-j-1
 
-                data_new_row,_ = self.charge_artifact_FD_filter_downup_av_prevlines3_irow_pass(data_filtered, data_labels_2d, irow, pass_dir0=pass_dir)
+                data_new_row,err, opts0 = self.charge_artifact_FD_filter_downup_av_prevlines3_irow_pass(data_filtered, data_labels_2d, irow, pass_dir0=pass_dir)
 
                 #Replace scan line with filtered
                 data_filtered[irow,:] = data_new_row
 
+                #opts0.append( pass_dir) #append info of the passdir to the optimisation values
+                if len(opts0)>0:
+                    opts.append([pass_dir, opts0]) # Collect optimisation values to a large array
 
         #print("slice complete")
-        return data_filtered.astype(data_2d.dtype)
+        return data_filtered.astype(data_2d.dtype), opts
 
     def charge_artifact_suppression_filter_3d(self, data3D, charge_center_labels_3d, dilate_iter=1):
 
         data_all_filt = np.zeros_like(data3D)
+        optsz = []
         for iz in range(data3D.shape[0]):
             print(f"iz:{iz} / {data3D.shape[0]-1} ")
             data_slice = data3D[iz,:,:]
@@ -324,10 +333,14 @@ class cls_charge_artifact_suppression_filter():
             # dilate labels
             labels_slice_dilated = binary_dilation(labels_slice,iterations=dilate_iter).astype(labels_slice.dtype)
 
-            res0 = self.charge_artifact_FD_filter_downup_av_prevlines3_2d(data_slice,labels_slice_dilated)
+            res0, opts = self.charge_artifact_FD_filter_downup_av_prevlines3_2d(data_slice,labels_slice_dilated)
 
             data_all_filt[iz,:,:] = res0.astype(data3D.dtype)
 
-        return data_all_filt
+            #opts.append(iz)
+            if len(opts)>0:
+                optsz.append([iz,opts])
+
+        return data_all_filt, optsz
 
 
