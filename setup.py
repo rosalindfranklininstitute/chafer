ba863c33847094cb9a8228c45ce106a87797ce28
Luis Perdigao <perdigao1@yahoo.com>
2022-10-25 10:29:40 +0100

    correct readme

--- setup.py
+++ setup.py
@@ -22,9 +22,9 @@ this_directory = Path(__file__).parent
 long_description = (this_directory / "README.md").read_text()
 
 setup(
-    version = '0.2',
+    version = '0.3',
     name = 'chafer',
-    description = 'CHArge artiFact SupprEssion Tool for Scanning ElectRon Microscope Images',
+    description = 'CHArge artiFact supprEssion tool for scanning electRon microscope images',
     long_description=long_description,
     long_description_content_type='text/markdown',
     url = 'https://github.com/rosalindfranklininstitute/chafer',
