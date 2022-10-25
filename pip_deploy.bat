f76c36f9edc79342929768be51b3e8c5d47a7530
Luis Perdigao <perdigao1@yahoo.com>
2022-10-25 10:58:17 +0100

    update pip_deploy.bat

--- pip_deploy.bat
+++ pip_deploy.bat
@@ -19,8 +19,11 @@ REM Deploy package to Pypi
 rem updates
 python -m pip install --upgrade setuptools wheel --user
 
+rmdir /s /q dist
+
 rem This will create two files in /dist folder: a .whl and a .tar.zip (.gz) file with the package
-python setup.py sdist bdist_wheel
+pip install --upgrade build
+python -m build
 
 rem updates
 pip install --upgrade twine --user
